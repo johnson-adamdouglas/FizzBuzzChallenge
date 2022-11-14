43bd42bdc3debc1a2208e961de1f9a905751dd5f
johnson-adamdouglas <115263791+johnson-adamdouglas@users.noreply.github.com>
2022-11-14 15:09:19 -0500

    Add files via upload

---
+++ FizzBuzzChallenge/FizzBuzzChallange/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FizzBuzzChallange
+{
+    internal class Program
+    {
+        public static string FizzBuzz(int num)
+        {
+            if (num % 15 == 0)
+            {
+                return "fizzbuzz";
+            }
+            else if (num % 5 == 0)
+            {
+                return "buzz";
+            }
+            else if (num % 3 == 0)
+            {
+                return "fizz";
+            }
+            else return "N/A";
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FizzBuzz(75));
+        }
+    }
+}
